8a16985946a846adb66a608f00f61827357b6424
sunyufishcat <2451204343@qq.com>
2021-10-27 12:25:23 +0800

    [sunyu] refactor todolist

--- src/component/TodoList/TodoList.js
+++ src/component/TodoList/TodoList.js
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react';
 import {dispatchContext} from '../Todos/Todos';
+import ListItem from './ListItem';
 import InputField from '../InputField/InputField';
 import OverView from '../OverView/OverView';
-import {toggleAll, inputDelete, buttonDelete} from '../../actions/dispatcher';
+import {toggleAll} from '../../actions/dispatcher';
 
 const TodoList = () => {
   const todoListContext = useContext(dispatchContext);
@@ -10,15 +11,6 @@ const TodoList = () => {
   
   const handleAllToggle = () => {
     toggleAll(todosDispatch, todos);
-  }
-  
-  const handleInputDelete = (id) => {
-    inputDelete(todosDispatch, id);
-    
-  }
-  
-  const handleButtonDelete = (id) => {
-    buttonDelete(todosDispatch, id);
   }
   
   return (
@@ -34,26 +26,9 @@ const TodoList = () => {
             htmlFor="toggle-all"
           />
           <ul className="todo-list">
-            {todos.map((todoItem, index) => (
-              <li key={index} className={todoItem.isCompleted ? "completed" : "active"}>
-                <div className="view">
-                  <InputField
-                    type="checkbox"
-                    id="item"
-                    onClick={() => {handleInputDelete(todoItem.id)}}
-                    className="item"
-                    labelValue={todoItem.value}
-                  />
-                  <button
-                    className="delete"
-                    onClick={() => handleButtonDelete(todoItem.id)}
-                  >
-                    ×
-                  </button>
-                </div>
-                <input className="edit"/>
-              </li>
-            ))}
+            {todos.map((todoItem, index) =>
+              <ListItem key={index} todoItem={todoItem}/>
+            )}
           </ul>
         </section>
       
--- src/component/TodoList/TodoList.test.js
+++ src/component/TodoList/TodoList.test.js
@@ -4,7 +4,7 @@ import {dispatchContext} from '../Todos/Todos';
 import TodoList from './TodoList';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
-import {toggleAll, inputDelete, buttonDelete} from '../../actions/dispatcher';
+import {toggleAll} from '../../actions/dispatcher';
 
 jest.mock('../../actions/dispatcher');
 const mockProvider = {
@@ -25,28 +25,10 @@ describe('TodoList', () => {
   
   test('should render all todo lists', () => {
     expect(screen.getAllByRole('listitem').length).toBe(2);
-    expect(screen.getByText('test1')).toBeInTheDocument();
-    expect(screen.getByText('test2')).toBeInTheDocument();
-    userEvent.hover(screen.getAllByRole('listitem')[0]);
-    expect(screen.getAllByRole('button')[0]).toHaveClass('delete');
   })
   
   test('should call toggle_all function when click toggle checkbox', () => {
     userEvent.click(screen.getAllByRole('checkbox')[0]);
     expect(toggleAll).toBeCalledWith(mockProvider.todosDispatch, mockProvider.todos);
-  })
-  
-  test('should call input_delete function when click todo item checkbox', () => {
-    userEvent.click(screen.getAllByRole('checkbox')[1]);
-    expect(inputDelete).toBeCalledWith(mockProvider.todosDispatch, 1);
-    userEvent.click(screen.getAllByRole('checkbox')[2]);
-    expect(inputDelete).toBeCalledWith(mockProvider.todosDispatch, 2);
-  })
-  
-  test('should call button_delete function when click delete button', () => {
-    userEvent.click(screen.getAllByRole('button')[0]);
-    expect(buttonDelete).toBeCalledWith(mockProvider.todosDispatch, 1);
-    userEvent.click(screen.getAllByRole('button')[1]);
-    expect(buttonDelete).toBeCalledWith(mockProvider.todosDispatch, 2);
   })
 })
